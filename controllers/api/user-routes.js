c32f9410605b212efb0c50fb8c3e27f1ff4f6c3f
Lyly Long <lylylong@gmail.com>
2020-12-05 13:15:11 -0500

    testing user get & post routes

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,32 @@
+const router = require("express").Router();
+const { User } = require("../../models");
+
+// GET /api/users
+router.get("/", (req, res) => {
+  User.findAll({
+    // protect password
+    attributes: { exclude: ["password"] },
+  })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// POST /api/users
+router.post("/", (req, res) => {
+  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const routes = require("./controllers/");
 const sequelize = require("./config/connection");
 
 const app = express();
@@ -7,6 +7,11 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// turn on routes
+// this must below the other two app.use!
+// the user post request wont read the username if this above the other two app.use
+app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
