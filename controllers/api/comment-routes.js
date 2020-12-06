d4b2aba8745802ef1ae9ea962d8877b4ade09d06
Lyly Long <lylylong@gmail.com>
2020-12-05 22:05:48 -0500

    the comment model and route testings

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,56 @@
+const router = require("express").Router();
+const { Post, User, Comment } = require("../../models");
+
+router.get("/", (req, res) => {
+  Comment.findAll({
+    attributes: ["id", "comment_text", "created_at"],
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
+      },
+      {
+        model: Post,
+        attributes: ["title"],
+      },
+    ],
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post("/", (req, res) => {
+  Comment.create({
+    comment_text: req.body.comment_text,
+    user_id: req.body.user_id,
+    post_id: req.body.post_id,
+  })
+    .then((dbCommentData) => res.json(dbCommentData))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+// router.post("/", (req, res) => {
+//   // check the session
+//   if (req.session) {
+//     Comment.create({
+//       comment_text: req.body.comment_text,
+//       post_id: req.body.post_id,
+//       // use the id from the session
+//       user_id: req.session.user_id,
+//     })
+//       .then((dbCommentData) => res.json(dbCommentData))
+//       .catch((err) => {
+//         console.log(err);
+//         res.status(400).json(err);
+//       });
+//   }
+// });
+
+router.delete("/:id", (req, res) => {});
+
+module.exports = router;
